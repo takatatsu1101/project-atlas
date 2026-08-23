59930ae1ed95eefbb84744831d410861c36311f8
takatatsu1101 <takatatsu.1101@gmail.com>
2026-08-23 20:04:56 +0900

    feat: add common exception classes, logging module, unit tests, and update plan

---
+++ src/common/__init__.py
@@ -0,0 +1,9 @@
+from src.common.logger import get_logger
+from src.common.exceptions import (
+    AtlasError,
+    DataCollectionError,
+    IndicatorCalculationError,
+    FeatureCalculationError,
+    PatternDetectionError,
+    ScoreCalculationError,
+)
